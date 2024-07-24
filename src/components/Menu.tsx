b51669acbea3a52cc2643a89ccb068766a1e1e6b
Eric <e.k.chi7@gmail.com>
2024-07-24 17:29:26 -0400

    setting up navbar links

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -34,7 +34,7 @@ const Menu = () => {
                 height={20}
                 onClick={()=>setOpen(false)}/>
             )}
-        { open && (
+        {open && (
             <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
             {links.map(item=>(
                 <Link href={item.url} key={item.id} onClick={()=>setOpen(false)}>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,39 @@
 import React from "react";
 import Menu from "./Menu";
 import Link from "next/link";
+import Image from "next/image";
+import CartIcon from "./CartIcon";
 
 const Navbar = () => {
+
+    const user = false
     return (
         <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+            {/* LEFT LINKS */}
+            <div className="hidden md:flex gap-4">
+                <Link href="/">Homepage</Link>
+                <Link href="/menu">Menu</Link>
+                <Link href="/">Contact</Link>
+            </div>
             {/* Logo */}
             <div className="text-xl">
                 <Link href="/">Massimo</Link>
             </div>
             {/* MobileMenu */}
-            <div>
+            <div className="md:hidden">
                 <Menu/>
+            </div>
+            {/* RIGHT LINKS */}
+            <div className="hidden md:flex gap-4">
+                <div>
+                    <Image src="/phone.png" alt=""/>
+                </div>
+                {!user ? (
+                    <Link href="/">Login</Link>
+                ) : (
+                    <Link href="/orders">Order</Link>
+                )}
+                <CartIcon />
             </div>
         </div>
     )
