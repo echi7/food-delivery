0f8c2c70be1228920595d779896c4957f0fe8f77
Eric <e.k.chi7@gmail.com>
2024-07-24 18:52:46 -0400

    fixing up navbar and making the phone number change position dependent on screen size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,15 +8,15 @@ const Navbar = () => {
 
     const user = false
     return (
-        <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+        <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24">
             {/* LEFT LINKS */}
-            <div className="hidden md:flex gap-4">
+            <div className="hidden md:flex gap-4 flex-1">
                 <Link href="/">Homepage</Link>
                 <Link href="/menu">Menu</Link>
                 <Link href="/">Contact</Link>
             </div>
             {/* Logo */}
-            <div className="text-xl">
+            <div className="text-xl md:font-bold flex-1 md:text-center">
                 <Link href="/">Massimo</Link>
             </div>
             {/* MobileMenu */}
@@ -24,9 +24,10 @@ const Navbar = () => {
                 <Menu/>
             </div>
             {/* RIGHT LINKS */}
-            <div className="hidden md:flex gap-4">
-                <div>
-                    <Image src="/phone.png" alt=""/>
+            <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+                <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+                    <Image src="/phone.png" alt="" width={20} height={20}/>
+                    <span>123 456 7890</span>
                 </div>
                 {!user ? (
                     <Link href="/">Login</Link>
