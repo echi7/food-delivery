2b8ceac70764e82b8ea575e20cb9f88d97312720
Eric <e.k.chi7@gmail.com>
2024-07-24 18:54:20 -0400

    creating padding the edges of the nav dependent on the screen size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     const user = false
     return (
-        <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24">
+        <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
             {/* LEFT LINKS */}
             <div className="hidden md:flex gap-4 flex-1">
                 <Link href="/">Homepage</Link>
