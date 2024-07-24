390ca312642151bd0f5487266deebf1e1e729266
Eric <e.k.chi7@gmail.com>
2024-07-24 16:11:07 -0400

    creating components for global html: navbar, notifcations, and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import Notification from "@/components/Notification";
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Notification />
+        <Navbar />
+        {children}</body>
+        <Footer />
     </html>
   );
 }
