b9d9846a1baa9edc9f787d8f143817a828081ef1
Eric <e.k.chi7@gmail.com>
2024-07-26 18:53:05 -0400

    updated the countdown component

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import CountDown from "./CountDown";
 
 const Offer = () => {
     return (
@@ -11,6 +12,7 @@ const Offer = () => {
             Progressively simplify effective e-toilers and process-centric methods
             of empowerment. Quickly pontificate parallel.
           </p>
+          <CountDown/>
           <button className="bg-red-500 text-white rounded-md py-3 px-6">Order Now</button>
         </div>
         {/* IMAGE CONTAINER */}
