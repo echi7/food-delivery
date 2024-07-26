c36874d6781a0176dc14081c895983ced07e2641
Eric <e.k.chi7@gmail.com>
2024-07-26 18:20:11 -0400

    starting to complete the featured wrapper and inserted data into the data.ts to have a list of products to show in the featured wrapper

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import Image from "next/image";
 
 const Featured = () => {
     return (
-        <div>Featured</div>
+        <div className="w-screen overflow-x-scroll text-red-500">
+            {/* WRAPPER */}
+            <div className="w-max flex">
+                {/* SINGLE ITEM */}
+                <div className="w-screen h-[60vh] flex flex-col items-center justify-around p-4">
+                    {/* IMAGE CONTAINER */}
+                    <div className="relative flex-1 w-full">
+                        <Image src="/temporary/p1.png" alt="" fill className="object-conatin"/>
+                    </div>
+                    {/* TEXT CONTAINER */}
+                    <div className="flex-1 flex flex-col gap-4">
+                        <h1 className="text-xl font-bold uppercase">Title</h1>
+                        <p>Desc</p>
+                        <span className="text-xl font-bold">123</span>
+                        <button className="bg-red-500 text-white p-2 rounded-md">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
