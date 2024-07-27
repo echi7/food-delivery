2d48dfb172ff4889af6b25073f6002e4f7c8d527
Eric <e.k.chi7@gmail.com>
2024-07-26 23:10:23 -0400

    fixed the featured to display tenths decimal place and single product now displays the total instead of a fixed price allowing for price to change with different selections

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -21,7 +21,7 @@ useEffect(() => {
 
     return (
         <div className="flex flex-col gap-4">
-            <h2 className="text-2xl font-bold">${price.toFixed(2)}</h2>
+            <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
             {/* OPTIONS CONTAINER */}
             <div className="flex gap-4">
                 {options?.map((option, index) => (
