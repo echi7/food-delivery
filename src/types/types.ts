4fcc946287de64fc700f948866de6f856fbbfb4e
Eric <e.k.chi7@gmail.com>
2024-08-10 01:20:32 -0400

    changing is featured on the home page to pull from the db and hooking up the Producttype in the global types file

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,12 @@ export type Menutype = {
     img?: string;
     color: string;
 }[];
+
+export type ProductType = {
+    id: string;
+    title: string;
+    desc?: string;
+    img?: string;
+    price: number;
+    options?: {title: string; additionalPrice: number}[];
+};
