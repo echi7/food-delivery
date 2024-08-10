a5ab288fd32114a72f0ad5aa38e2b2ed84d8d2fb
Eric <e.k.chi7@gmail.com>
2024-08-10 00:13:19 -0400

    saving changes

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,7 +1,20 @@
+import { PrismaClient } from "@prisma/client/extension"
 import { NextResponse } from "next/server"
 
-export const GET = () => {
-    return new NextResponse("hello", {status: 200})
+const prisma = new PrismaClient()
+
+// FETCH ALL CATEGORIES
+
+export const GET = async () => {
+    try {
+        const categories = await prisma.category.findMany()
+        return new NextResponse(JSON.stringify(categories), {status: 200});
+    } catch(err){
+        console.log(err)
+        return new NextResponse(JSON.stringify({message:"Something went wrong!"}),
+        {status: 500}
+    );
+    }
 }
 export const POST = () => {
     return new NextResponse("hello", {status: 200})
