b4df42529036d0204140d2070e5f29647405abb2
Eric <e.k.chi7@gmail.com>
2024-08-10 01:13:13 -0400

    grabbing products but filtering by the category slug, if its is the homepage then it will be the isfeatured

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,11 +1,19 @@
 import prisma from "@/utils/connect";
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 // FETCH ALL PRODUCTS
-export const GET = async () => {
+export const GET = async (req: NextRequest) => {
+
+    const {searchParams} = new URL(req.url)
+    const cat = searchParams.get('cat')
+
     try {
-        const categories = await prisma.category.findMany()
-        return new NextResponse(JSON.stringify(categories), {status: 200});
+        const products = await prisma.product.findMany({
+            where: {
+                ...(cat ? { catSlug: cat } : { isFeatured: true }),
+            },
+        });
+        return new NextResponse(JSON.stringify(products), {status: 200});
     } catch(err){
         console.log(err)
         return new NextResponse(JSON.stringify({message:"Something went wrong!"}),
