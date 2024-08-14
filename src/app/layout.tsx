5c54fef6bfa283465b71edbcce4f284738d76a1c
Eric <e.k.chi7@gmail.com>
2024-08-14 19:46:59 -0400

    making user authentication and role based authorization with orders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
+import QueryProvider from "@/components/QueryProvider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,12 +23,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
+          <QueryProvider>
           <div>
             <Notification />
             <Navbar />
             {children}
             <Footer />
           </div>
+          </QueryProvider>
         </AuthProvider>
         </body>
     </html>
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,10 +1,44 @@
-import NextAuth from "next-auth"
+import NextAuth, { User } from "next-auth"
 import google from "next-auth/providers/google"
 import facebook from "next-auth/providers/facebook"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "./connect"
 
+declare module "next-auth"{
+    interface Session {
+        user: User & {
+            isAdmin: Boolean;
+        }
+    }
+}
+declare module "@auth/core/jwt"{
+    interface JWT {
+        isAdmin: Boolean;
+
+    }
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
+    session: {
+        strategy:"jwt"
+    },
     providers: [ google, facebook ],
+    callbacks:{
+        async session({token, session}) {
+            if(token) {
+                session.user.isAdmin = token.isAdmin;
+            }
+            return session
+        },
+        async jwt({token}){
+            const userInDb = await prisma.user.findUnique({
+                where: {
+                    email: token.email!
+                }
+            })
+            token.isAdmin = userInDb?.isAdmin!;
+            return token
+        }
+    }
 })
