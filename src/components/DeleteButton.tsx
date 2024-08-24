a28ee36e3245b92d063e166212d4ec09d967ccb9
Eric <e.k.chi7@gmail.com>
2024-08-24 19:00:53 -0400

    making it use authenticated page

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,8 +1,25 @@
 "use client"
 
+import { useSession } from "next-auth/react"
+import Image from "next/image"
+import { useRouter } from "next/navigation";
+
 const DeleteButton = () => {
+const {data:session, status} = useSession()
+const router = useRouter()
+
+if(status ==="loading") {
+    return <p>Loading...</p>;
+}
+
+if (status === "unauthenticated" || !session?.user.isAdmin) {
+    return;
+}
+
     return (
-        <button className="bg-red-400 p-2 rounded-full absolute top-4 right-4">DeleteButton</button>
+        <button className="bg-red-400 p-2 rounded-full absolute top-4 right-4">
+            <Image src="/delete.png" alt="" width={20} height={20}/>
+        </button>
     )
 }
 
