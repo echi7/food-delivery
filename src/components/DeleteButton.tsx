889bde7cc5313e783aa57f907bb0f629e98db6ab
Eric <e.k.chi7@gmail.com>
2024-08-26 00:55:35 +0200

    downloading stripe libraries and adding id and secret in env

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -3,8 +3,9 @@
 import { useSession } from "next-auth/react"
 import Image from "next/image"
 import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
-const DeleteButton = () => {
+const DeleteButton = ({id}:{id:string}) => {
 const {data:session, status} = useSession()
 const router = useRouter()
 
@@ -16,8 +17,20 @@ if (status === "unauthenticated" || !session?.user.isAdmin) {
     return;
 }
 
+const handleDelete = async () => {
+const res = await fetch(`http://localhost:3000/api/products/${id}`, {
+    method: "DELETE"
+})
+if(res.status === 200){
+    router.push("/menu")
+    toast("The product has been deleted!")
+}else{
+    const data = await res.json()
+    toast.error(data.message);
+}
+}
     return (
-        <button className="bg-red-400 p-2 rounded-full absolute top-4 right-4">
+        <button className="bg-red-400 p-2 rounded-full absolute top-4 right-4" onClick={handleDelete}>
             <Image src="/delete.png" alt="" width={20} height={20}/>
         </button>
     )
